6e38a7ade165aea1037400e7afee1f8c0cf34ebe
santos colque <82119113+sn-efa@users.noreply.github.com>
2022-06-22 22:39:49 -0400

    (codigo) conexion mysql. prueba mysql

---
+++ pruebamysql/src/main/java/com/mycompany/pruebamysql/Pruebamysql.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pruebamysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Colque
+ */
+public class Pruebamysql {
+
+    
+    String bd = "bddecaseritos";
+    String url = "jdbc:mysql://localhost:3306/";
+    String user = "root";
+    String password = "decaseritos";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection cx;
+    
+    
+    public Pruebamysql() {
+
+    }
+
+    public Connection conectar() {
+       try {
+           Class.forName(driver);
+           cx = DriverManager.getConnection(url + bd, user, password);
+           System.out.println("se conecto a base de datos: " + bd);
+       } catch (ClassNotFoundException | SQLException ex) {
+           System.out.println("no se conecto a base de datos: " + bd);
+           Logger.getLogger(Pruebamysql.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return cx;
+   }
+    public void desconectar() {
+        try {
+            cx.close();
+        } catch (SQLException ex) {
+           Logger.getLogger(Pruebamysql.class.getName()).log(Level.SEVERE, null, ex);
+       }
+  }
+    
+    public static void main(String[] args) {
+       Pruebamysql conexion = new Pruebamysql();
+      conexion.conectar();
+    }
+}
